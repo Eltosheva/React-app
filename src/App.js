0c1dbfbebb0833f4d74fd31b52e644c477a3adbd
ElTosheva <elisaveta.tosheva@gmail.com>
2021-12-01 09:44:05 +0200

    add styles
    ...

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+
+import { Switch, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
@@ -7,20 +9,19 @@ import Add from './components/Add/Add';
 function App() {
   return (
     <div>
-       <Header/>
-       
-       <main id="site-content"> 
-       <Home/>
-       <Login/>
-       <Register/>
-       <Add/>
+     <Header/>
+     <main id="site-content">
+       <switch>
+         <Route path="/home" components={Home}></Route>
+         <Login/>
+         <Register/>
+         <Add/>
+       </switch>
        </main>
 
        <footer id="site-footer">
            <p>@Delishes</p>
        </footer>
-
-     
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,14 +3,17 @@ import { Link } from 'react-dom';
 const Header = () => {
 return(
     <header>
-        <h2>DeliSHesss</h2>
+        <h2><a className="home" href='/home'>DeliSHesss</a></h2>
         <nav>
             <ul class="navigation">
-                <li>MyDelishes</li>
-                <li>Add NEW</li>
-                <li>Register</li>
-                <li>Login</li>
-                <li>Logout</li>   
+                <div id='user'>
+                    <li><a href='/add'>Add NEW</a></li>
+                    <li><a href='/logout'>Logout</a></li>
+                </div>
+                <div id='guest'>
+                    <li><a href='/register'>Register</a></li>
+                    <li><a href='/login'>Login</a></li>
+                </div>
             </ul>
         </nav>
     </header>
